0d5f3df2d1b2bd29e06c0721973b135c93bb97a7
arywd5 <arywd5@mail.missouri.edu>
2018-04-05 13:47:36 -0500

    changes made

--- workspace/lab5/lab5.c
+++ workspace/lab5/lab5.c
@@ -4,6 +4,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+
+//IP 128.206.19.255
 
 int main(){
 
