1f6dd7311932bf9fd3dc218685e2851d3c97d2f2
arywd5 <arywd5@mail.missouri.edu>
2018-04-11 17:45:37 -0500

    finished and commented most of the code

--- workspace/lab5/lab5.c
+++ workspace/lab5/lab5.c
@@ -30,10 +30,11 @@ int main(int argc, char *argv[]){
 	char message[MSG_SIZE], myIP[NI_MAXHOST];
 	struct ifaddrs *ifaddr, *ifa;	
 	struct sockaddr_in server, from;
-	char otherIP[2], otherVotes;
+	char otherIP[2], *otherVotes, WS[2];
+	otherVotes = (char *)malloc(sizeof(char));
 	srand(time(0));
 
-	
+
 	//get IP of our network
 	if((getifaddrs(&ifaddr)) == -1){
 		printf("\nError in getifaddrs");
@@ -45,17 +46,20 @@ int main(int argc, char *argv[]){
 
 		var = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), myIP, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);		
 	}	
+	WS[0] = myIP[11];			//Save work station number so we can use to compare later in the program 
+	WS[1] = myIP[12];
 
-	printf("\nMy IP is: %s", myIP);
+	printf("\nMy IP is: %s", myIP);		//print IP to the user 
 
 	//create socket 
 	if((soc = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
 		printf("\nSocket error..");
 		return -1;
 	}
-
+	
+	//set up sockaddr structure to use for sending and recieving messages
 	from.sin_family = AF_INET;
- 	from.sin_port = htons(atoi(argv[1]));
+	from.sin_port = htons(atoi(argv[1]));
 	inet_aton("128.206.19.255", &from.sin_addr);
 	length  = sizeof(struct sockaddr_in);
 
@@ -64,9 +68,9 @@ int main(int argc, char *argv[]){
 
 	//broadcast and bind socket 
 	if(bind(soc, (const struct sockaddr *)&from, sizeof(from)) < 0){
-                printf("\nError binding socket..");
-                return -1;
-        }
+		printf("\nError binding socket..");
+		return -1;
+	}
 
 	var = setsockopt(soc, SOL_SOCKET, SO_BROADCAST, &bbroad, sizeof(bbroad));
 	if(var < 0){
@@ -76,64 +80,79 @@ int main(int argc, char *argv[]){
 
 	//after socket is created enter an infinite loop to read in messages and send messages
 	while(1){
-		
+		memset(message,'\0', 40);			//clear message so we dont have any leftover numbers in it 
 		var = recvfrom(soc, message, MSG_SIZE, 0, (struct sockaddr *)&from, &length);
-		printf("\nMessage Recieved: %s", message);
-		if(var < 0){
+		printf("\nMessage Recieved: %s", message);	//print recieved messafge to the screen 
+		if(var < 0){					//check that message was recieved properly 
 			printf("\nError Recieving Message..");
 			return -1;
 		}
 		//WHOIS recieved 
 		else if(strncmp(message, "WHOIS", 5) == 0){
-			if(master == 1){
-				sprintf(message, "Allie at %s if master", myIP);
+			if(master == 1){			//check if the master flag is set and if so send message 
+				sprintf(message, "Allie at %s is master", myIP);
 				var = sendto(soc, message, strlen(message), 0, (struct sockaddr *)&from, length);			
 			}
 		}
 		//VOTE recieved
 		else if(strncmp(message, "VOTE", 4) == 0){
-			votes = (rand()%10) + 1;		
-			sprintf(message, "#%s %d", myIP, votes);
+			votes = (rand()%10) + 1;		//generate random vote number between 1-10	
+			sprintf(message, "# %s %d", myIP, votes);	//then send vote 
 			var = sendto(soc, message, strlen(message), 0, (struct sockaddr *)&from, length);
-			flags[2] = 1;	
 		}
 		//IP # of votes recieved 
-		else if(strncmp(message, "# 128.206.19", (size_t)(12*sizeof(char)))){
-			int getIP = 0, getVote = 0;
-			for(i = 12; message[i] != '\0'; i++){ //start parsing at position 12 because we know its the same up until then 
-				if(message[i] = '.'){
-					getIP = 1;	
+		else if(strncmp(message, "# 128.206.19", (size_t)(12*sizeof(char))) == 0){
+			int getIP = 0, getVote = 0;	//flags to use when parsing the message string 
+			for(i = 12; message[i - 2] != ' '; i++){ //start parsing at position 12 because we know its the same up until then 
+				if(message[i] == '.'){		//if we read a . then next number will be the IP or workstation number 
+					getIP = 1;		//so we set the flag 
 				}
-				else if(message[i] = ' '){
-					getIP = 0;
-					getVote = 1;
+				else if(message[i] == ' '){	//if a space is read the next number will be the number of votes 
+					getIP = 0;		//set getIP glad to zero 
+					getVote = 1;		//and set getVote flag
 				}
 				else if(getIP > 0){		//if the get IP flag is set that means we found a . and the IP should follow it 
-					otherIP[getIP - 1] = message[i];
+					otherIP[getIP - 1] = message[i];	//store next value in our otherIP variable 
 					getIP++;
 				}
 				else if(getVote = 1){		//if get vote flag is on save this to our number of votes
-					otherVotes = message[i];	
+					*otherVotes = message[i];	//if vote flag is set get number of votes 
+					getVote = 0;
 				}
 			}
 			//now that we have parsed the message and got the IP number and vote number we can compare ours 
-			if(atoi(&otherVotes) == votes){
+			if(atoi(otherVotes) == votes){
 				//need to compare the IP's
 				printf("\nComparing IPs...");
-				
+				int theirs = atoi(otherIP), mine = atoi(WS);
+				if(theirs == 0){		
+					theirs = atoi(&(otherIP[0]));				
+				}	
+				if(theirs != 0){
+					if(mine > theirs){
+						master = 1;
+						sprintf(message, "Allie at %s is master", myIP);
+						var = sendto(soc, message, strlen(message), 0, (struct sockaddr *)&from, length);
+					}	
+					else{
+						master = 0;
+					}
+				}
+
 			}
-			else if(atoi(&otherVotes) <= votes){
+			else if(atoi(otherVotes) < votes){
 				//you are the master so send the message 
 				master = 1;
 				sprintf(message, "Allie at %s is master", myIP);
 				var = sendto(soc, message, strlen(message), 0, (struct sockaddr *)&from, length);
 			}
 			else{ 	//you are not the master dont send any messages 
-				master = 0; 	
-				printf("\nYou are not the master..");
+				master = 0; 		
 			}
 		}
 	}
 
+	free(otherVotes);
 	return 0;
 }
+
